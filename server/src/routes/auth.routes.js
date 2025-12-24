c37982e682ba3b195135b052328c84c7c0c8ca58
jej3zin <127148233+jej3zin@users.noreply.github.com>
2025-12-24 16:53:32 +0000

    auth routes  ajustes api

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { login, register } from '../controllers/auth.controller.js';
 import { auth } from '../middleware/auth.middleware.js';
+import { me } from '../controllers/auth.controller.js'; // caminho correto pro seu controller
+import { refresh } from '../controllers/auth.controller.js';
 
 const router = Router();
 
