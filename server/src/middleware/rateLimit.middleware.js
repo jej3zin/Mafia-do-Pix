a7524a53a19cd9a4432008eeb33fa2cd7eb82309
jej3zin <127148233+jej3zin@users.noreply.github.com>
2026-01-14 13:50:57 +0000

    "options"

--- server/src/middleware/rateLimit.middleware.js
+++ server/src/middleware/rateLimit.middleware.js
@@ -1,6 +1,8 @@
+// server/src/middleware/rateLimit.middleware.js
 import rateLimit from 'express-rate-limit';
 
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
+  skip: (req) => req.method === 'OPTIONS',
 });
